6636eae47db23c0d7f50a23caf47cc161ae64488
Alice (ManucharovVD) <manu4arov@example.com>
2023-07-05 19:08:57 +0300

    code:заготовка программы

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	cout << "Enter A and B: ";
+	double a, b;
+	cin >> a >> b;
+
+	cout << "A + B = " << a + b << '\n'
+		<< "A - B = " << a - b << '\n'
+		<< "A * B = " << a * b << '\n'
+		<< "A / B = " << a / b << '\n';
+
+	if (a < b) {
+		cout << b;
+	}
+	else {
+		cout << a;
+	}
+	if (a > b) {
+		cout << a;
+	}
+	else {
+		cout << b;
+	}
+	return 0;
+}
